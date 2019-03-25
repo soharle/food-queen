caca63a7e9fd524abc70fbca1dad6579210846d9
soharle <matheus7rod@gmail.com>
2019-03-24 21:02:06 -0300

    front controller

---
+++ src/java/controller/ActionFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import controller.Action;
+
+public class ActionFactory {
+    public static Action create(String action) {
+        Action actionObject = null;
+        String nomeClasse = "action." + action + "Action";
+        Class classe = null;
+        Object objeto = null;
+        try {
+            classe = Class.forName(nomeClasse);
+            objeto = classe.newInstance();
+        } catch (Exception ex) {
+            return null;
+        }
+        if (!(objeto instanceof Action)) {
+            return null;
+        }
+        actionObject = (Action) objeto;
+        return actionObject;
+    }
+}
