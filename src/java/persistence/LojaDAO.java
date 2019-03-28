725f4704914aee72a30616fdcc03b047b0c0a365
gabetto <gabrielbeto83@gmail.com>
2019-03-28 19:25:05 -0300

    fix dao de loja

--- src/java/persistence/LojaDAO.java
+++ src/java/persistence/LojaDAO.java
@@ -79,10 +79,10 @@ public class LojaDAO {
             st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT loja.*, conta.*, contato.*, endereco_loja.*, categoria.* "
                     + "FROM loja "
-                    + "INNER JOIN conta ON loja.conta_id = conta.id"
-                    + "INNER JOIN contato ON loja.contato_id = contato.id"
-                    + "INNER JOIN endereco_loja ON loja.endereco_loja_id = endereco_loja.id"
-                    + "INNER JOIN categoria ON loja.categoria_id = categoria.id");
+                    + "INNER JOIN conta ON loja.conta_id = conta.id "
+                    + "INNER JOIN contato ON loja.contato_id = contato.id "
+                    + "INNER JOIN endereco_loja ON loja.endereco_loja_id = endereco_loja.id "
+                    + "INNER JOIN categoria ON loja.categoria_id = categoria.id;");
             while (rs.next()) {
                 Conta conta = new Conta(rs.getLong("conta.id"), rs.getString("conta.login"),
                         rs.getString("conta.senha"), rs.getString("conta.tipo"));
